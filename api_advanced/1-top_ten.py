af7fbc5c7e783ff2c5201dcab84484ceea6dd6a9
EliseJulio <j.hakiziman1@alustudent.com>
2024-10-26 23:52:56 +0200

    All Tasks

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,10 +8,10 @@ def top_ten(subreddit):
     reddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url, headers=headers)
-    ok = "OK"
+    
     if response.status_code == 200:
         data = response.json()['data']
         for post in data['children'][:10]:
             print(post['data']['title'])
     else:
-        print(ok)
+        print(None)
